c5dc52de5bffd70d7852c6d8141ac20c98b3a010
itsbot <patrickiannini@knights.ucf.edu>
2021-09-14 18:36:07 -0400

    Initial commit

---
+++ src/main/java/Exercise24/Exercise24.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 24 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package Exercise24;
+
+public class Exercise24 {
+    public static void main(String[] args) {
+        
+    }
+}
