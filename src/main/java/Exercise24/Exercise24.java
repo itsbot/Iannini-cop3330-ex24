fd0f67beefc02f125f3011b9684c7361a37b6e29
itsbot <patrickiannini@knights.ucf.edu>
2021-09-22 18:22:04 -0400

    Completed Exercise

--- src/main/java/Exercise24/Exercise24.java
+++ src/main/java/Exercise24/Exercise24.java
@@ -3,9 +3,43 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise24;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Exercise24 {
+    // Function first checks length before converting to char[], sorting, and checking if letters match.
+    public static boolean isAnagram(String input1, String input2) {
+        if (input1.length() != input2.length()) {
+            return false;
+        }
+
+        char[] in1 = input1.toCharArray();
+        char[] in2 = input2.toCharArray();
+        Arrays.sort(in1);
+        Arrays.sort(in2);
+
+        for (int i=0; i<input1.length(); i++) {
+            if (in1[i] != in2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two strings and I'll tell you if they are anagrams.");
+        System.out.println("Enter the first string: ");
+        String input1 = sc.next();
+        System.out.println("Enter the second string: ");
+        String input2 = sc.next();
+
+        boolean anagram = isAnagram(input1, input2);
+
+        if (anagram) {
+            System.out.println("\""+input1+"\" and "+"\""+input2+"\" are anagrams.");
+        } else System.out.println("\""+input1+"\" and "+"\""+input2+"\" are not anagrams.");
+
+        sc.close();
     }
 }
